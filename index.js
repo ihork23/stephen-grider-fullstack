ca4ea558e9dc304a514dd6e21d95326fa49f45cf
ihork <js.web23@gmail.com>
2022-09-05 17:40:37 +0200

    Add cookie-session

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
 const authRoutes = require('./routes/authRoutes')
 const keys = require('./config/keys')
 
@@ -9,6 +11,15 @@ require('./services/passport')
 mongoose.connect(keys.mongoURI).then(() => console.log('success')).catch(e => console.log(e))
 
 const app = express()
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+)
+app.use(passport.initialize())
+app.use(passport.session())
 
 authRoutes(app)
 
--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,16 @@ const { googleClientID, googleClientSecret } = require('../config/keys')
 
 const User = mongoose.model('users')
 
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user)
+  })
+})
+
 passport.use(new GoogleStrategy({
   clientID: googleClientID,
   clientSecret: googleClientSecret,
