bff7986d474885d97c6afd3280e449db0d3f1a0e
ihork <js.web23@gmail.com>
2022-09-08 20:38:59 +0200

    Init login ui

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,17 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux'
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return 'null'
+      case false:
+        return (<a href="/auth/google">Login With Google</a>)
+      default:
+        return (<a href="/auth/google">Logout With Google</a>)
+    }
+  }
   render() {
     return (
       <nav>
@@ -10,7 +21,7 @@ class Header extends Component {
           </a>
           <ul className="right">
             <li>
-              <a>Login With Google</a>
+              {this.renderContent()}
             </li>
           </ul>
         </div>
@@ -19,4 +30,8 @@ class Header extends Component {
   }
 }
 
-export default Header
+function mapStateToProps({ auth }) {
+  return { auth }
+}
+
+export default connect(mapStateToProps)(Header)
