1c9d8467c141c1d8f2f2b35b07e2eb158443a7a0
ihork <js.web23@gmail.com>
2022-10-27 18:14:03 +0200

    Webhook on user voting action

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const { Path } = require('path-parser')
+const { URL } = require('url')
+const uniqBy = require('lodash/uniqBy')
 const mongoose = require('mongoose')
 const requireLogin = require('../middlewares/requireLogin')
 const requireCredits = require('../middlewares/requireCredits')
@@ -7,7 +10,37 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplates')
 const Survey = mongoose.model('surveys')
 
 module.exports = app => {
-  app.get('/api/surveys/thanks', (req, res) => res.send('Thanks for voting!'))
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => res.send('Thanks for voting!'))
+
+  app.post('/api/surveys/webhooks', (req, res) => {
+    const p = new Path('/api/surveys/:surveyId/:choice')
+    console.log('webhook')
+    const events = req.body
+      .map(({ url, email }) => {
+        const match = p.test(new URL(url).pathname)
+
+        if (match) {
+          return {email, surveyId: match.surveyId, choice: match.choice}
+        }
+      })
+      .filter(item => item !== undefined)
+    const uniqueEvents = uniqBy(events, 'email', 'surveyId')
+
+    uniqueEvents.forEach(({ surveyId, email, choice }) => {
+      Survey.updateOne({
+        _id: surveyId,
+        recipients: {
+          $elemMatch: {email: email, responded: false}
+        }
+      }, {
+        $inc: { [choice]: 1 },
+        $set: { 'recipients.$.responded': true },
+        lastResponded: new Date()
+      }).exec()
+    })
+
+    res.send({})
+  })
   
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     const {title, body, subject, recipients} = req.body
@@ -16,7 +49,7 @@ module.exports = app => {
       title,
       subject,
       body,
-      recipients: recipients.split(',').map(email => ({email: email.trim()})),
+      recipients: recipients.split(',').map(email => ({email: email.trim(), responded: false})),
       _user: req.user.id,
       dateSent: Date.now()
     })
